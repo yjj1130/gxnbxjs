8e46cac428264a15ba106ebfa2986826ca45c070
yjj1130 <787807497@qq.com>
2016-11-02 11:23:08 +0800

    gxnbxjs

--- PIcx.f90
+++ PIcx.f90
@@ -4,14 +4,14 @@ real(8),parameter :: pi=3.14159265358979323846D0
 real(8) :: mypi, h, mysum=0.0D0, x
 integer :: n, i
 
-n=1000000
+n=1000000000
 h=1.0D0/n
 do i=1, n
     x=h*(dble(i)-0.5D0)
     mysum=mysum+f(x)
 enddo
 mypi=h*mysum
-
+write(*,'(A10,F20.12)')"PI: ",mypi
 contains
     function f(x)
         real(8) :: f, x
